5cb9ed06ab9d83efa16519ed7f9027be96c57048
rinzuala-zostream <kawlkulhcentre@gmail.com>
2025-04-22 12:33:17 +0530

    Describe your changes

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     protected $table = 'user';
+    public $timestamps = false;
 
     protected $casts = [
         'isACActive' => 'boolean',
