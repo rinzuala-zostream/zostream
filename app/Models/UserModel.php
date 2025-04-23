b4f01d51fa60ed077a3aee8f70bb0180346afc2e
rinzuala-zostream <kawlkulhcentre@gmail.com>
2025-04-23 13:00:14 +0530

    Describe your changes

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -9,9 +9,27 @@ class UserModel extends Model
     protected $table = 'user';
     public $timestamps = false;
 
+    protected $fillable = [
+        'call',
+        'created_date',
+        'device_id',
+        'dob',
+        'edit_date',
+        'img',
+        'isACActive',
+        'isAccountComplete',
+        'khua',
+        'lastLogin',
+        'mail',
+        'name',
+        'uid',
+        'veng',
+        'device_name',
+        'token'
+    ];
+
     protected $casts = [
         'isACActive' => 'boolean',
         'isAccountComplete' => 'boolean',
     ];
-    
 }
