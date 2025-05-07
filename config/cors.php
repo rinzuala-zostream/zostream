4b4c673dc15996fa589b51dd8864aac035a62232
lalremruatakhiangte <lalremruatakhiangte656@gmail.com>
2025-05-07 16:41:06 +0530

    Describe your changes

--- config/cors.php
+++ config/cors.php
@@ -2,17 +2,11 @@
 
 return [
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'https://play.zostream.in',
-        'http://127.0.0.1:8000',
-        'http://192.168.137.159:5173', // if you're accessing from local network
-    ],
+    'allowed_origins' => ['https://play.zostream.in'],
 
     'allowed_origins_patterns' => [],
 
