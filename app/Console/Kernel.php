bd2859ff6653ec07b0909db058edf0788d918604
lalremruatakhiangte <lalremruatakhiangte656@gmail.com>
2025-05-08 10:42:04 +0530

    Describe your changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,16 +7,35 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        \App\Console\Commands\SendBirthdayWishes::class, // Register your custom command
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
     protected function schedule(Schedule $schedule)
     {
-        // Schedule your command to run daily between 7AM and 10AM
+        // Schedule the SendBirthdayWishes command to run every five minutes
         $schedule->command('app:send-birthday-wishes')->everyFiveMinutes();
-
     }
 
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-        require base_path('routes/console.php');
+        $this->load(__DIR__.'/Commands'); // Load commands from the Commands directory
+        require base_path('routes/console.php'); // Load route definitions
     }
 }
