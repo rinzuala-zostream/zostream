dd5fcf507cae4f19ca9bef4d047cade303423346
lalremruatakhiangte <lalremruatakhiangte656@gmail.com>
2025-06-04 12:35:13 +0530

    Describe your changes

--- routes/console.php
+++ routes/console.php
@@ -11,4 +11,4 @@ Schedule::command('birthday:send')->hourly()->between('9:00', '13:00');
 
 Schedule::command("app:queue-today-birthdays")->daily();
 
-Schedule::command('movie-schedule')->hourly()->between('12:00', '14:00');
+Schedule::command('app:movie-schedule')->hourly()->between('12:00', '14:00');
