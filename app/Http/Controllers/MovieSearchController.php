eee335bef5a0c3ba62ad2fd0d17bc2a13a06be75
lalremruatakhiangte <lalremruatakhiangte656@gmail.com>
2025-06-04 18:05:57 +0530

    Describe your changes

--- app/Http/Controllers/MovieSearchController.php
+++ app/Http/Controllers/MovieSearchController.php
@@ -22,7 +22,7 @@ class MovieSearchController extends Controller
         }
 
         $query = strtolower(trim(preg_replace('/\s+/', ' ', $request->query('q', ''))));
-        $ageRestriction = $request->query('age_restriction', 'false') ? 1 : 0; // Default to 'false'
+        $ageRestriction = $request->query('age_restriction', 'false') === 'true' ? 1 : 0; // Default to 'false'
         $isEnableRequest = $request->query('is_enable', 'true');
 
         if (empty($query)) {
