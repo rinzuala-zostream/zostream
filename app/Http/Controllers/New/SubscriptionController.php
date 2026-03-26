98ca6f546b09d06b3ed95a55a9c7160522201c1a
remruatakells <lalremruatakhiangte656@gmail.com>
2026-03-26 16:06:05 +0530

    update

--- app/Http/Controllers/New/SubscriptionController.php
+++ app/Http/Controllers/New/SubscriptionController.php
@@ -283,7 +283,7 @@ class SubscriptionController extends Controller
                     ], 400);
                 }
 
-                $transactionId = $razorpayData['order'];
+                $transactionId = $razorpayData['order']['id'];
             }
 
             /*
