23ffcb0e7f4ff1c5d551cddedf90a7dd69c8b4e4
remruatakells <lalremruatakhiangte656@gmail.com>
2026-04-08 13:55:53 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -258,6 +258,7 @@ Route::prefix('v3.0')->group(function () {
     });
 
     Route::prefix('movies')->group(function () {
+        Route::get('/alsolike', [\App\Http\Controllers\New\AlsoLikeController::class, 'alsoLike']);
         Route::get('/home', [\App\Http\Controllers\New\MovieController::class, 'getMovies']);
         Route::get('/search', [\App\Http\Controllers\New\SearchController::class, 'search']);
         Route::get('/details', [\App\Http\Controllers\New\DetailsController::class, 'getDetails']);
@@ -265,7 +266,7 @@ Route::prefix('v3.0')->group(function () {
         Route::get('/', [\App\Http\Controllers\New\MovieController::class, 'index'])->name('movies.index');
         Route::get('/{id}', [\App\Http\Controllers\New\MovieController::class, 'getById'])->name('movies.show');
         Route::get('/{id}/links', [\App\Http\Controllers\New\MovieController::class, 'getLink'])->name('movies.links');
-        Route::get('/alsolike', [\App\Http\Controllers\New\AlsoLikeController::class, 'alsoLike']);
+        
     });
 
     Route::prefix('payments')->group(function () {
